5cd6775a0ae61955987181b56ab1e12e28388006
LateBloomer34 <himnariyal30@gmail.com>
2026-04-13 11:10:15 +0530

    deletenode from LL

---
+++ 11A/LL/06DeleteLast.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+
+    Node (int val){
+        value = val;
+        next = NULL;
+    }
+};
+
+void iterarte(Node* head){
+    Node* temp = head ;
+   
+    while(temp!=nullptr ){
+        cout<<temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void delLast(Node* &head){
+    if (head==NULL){
+        return;
+    }
+    if (head->next==nullptr){
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* temp =head;
+
+    while(temp->next->next!=nullptr){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+
+} 
+
+
+int main (){
+    Node* node1 = new Node(2);
+    Node* node2 = new Node(3);
+    Node* node3 = new Node(4);
+    Node* node4 = new Node(5);
+    Node* node5 = new Node(6);
+    Node* node6 = new Node(7);
+
+    Node* head = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    
+    
+    delLast (head);
+    iterarte(head);
+}
