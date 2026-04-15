de7a48314cefd9a42492e1bd17970f5d49622f77
LateBloomer34 <himnariyal30@gmail.com>
2026-04-15 08:16:45 +0530

    delete specific

---
+++ 13A/LL/01deleteSpecific.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+
+    Node(int val){
+        value = val;
+        next = nullptr;
+    }
+};
+
+void iterate(Node* head){
+    Node* temp = head;
+
+    while(temp!=nullptr ){
+        cout<<temp->value<<" ";
+        temp= temp->next;
+    }
+    cout<<"..."<<endl;
+}
+
+void del (Node* &head , int val ){
+    if (head==nullptr){
+        
+    }
+    Node *temp = nullptr;
+
+
+}
+
+int main (){
+    Node *node1 = new Node (1);
+    Node *node2 = new Node (2);
+    Node *node3 = new Node (3);
+    Node *node4 = new Node (4);
+    Node *node5 = new Node (5);
+    Node *node6 = new Node (6);
+    Node *node7 = new Node (7);
+    Node *node8 = new Node (8);
+
+    Node* head= node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    node6->next = node7;
+    node7->next = node8;
+
+    del(head , 3);
+    iterate(head);
+}
