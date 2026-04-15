f40f1d955a595fa8b55cc0f6195f81fe616b2bb3
LateBloomer34 <himnariyal30@gmail.com>
2026-04-15 08:17:14 +0530

    LL pracice

---
+++ 15A/LL/01traverse.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+
+
+    Node(int val){
+        value = val;
+        next = nullptr;
+    }
+};
+
+void iterate(Node* head){
+    Node* temp = head;
+
+    while(temp!=nullptr){
+        cout<<temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
+
+int main (){
+    Node* node1 = new Node(1);
+    Node* node2 = new Node(2);
+    Node* node3 = new Node(3);
+    Node* node4 = new Node(4);
+    Node* node5 = new Node(5);
+    Node* node6 = new Node(6);
+
+    Node* head = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+
+    iterate(head);
+    
+}
