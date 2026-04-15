b51792adf1e08eb5987f855bd1fa816860bb2ada
LateBloomer34 <himnariyal30@gmail.com>
2026-04-15 08:20:55 +0530

    add in begning

---
+++ 15A/LL/02addBegning.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+
+
+    Node(int val){
+        value = val;
+        next = nullptr;
+    }
+};
+
+void iterate(Node* head){
+    Node* temp = head;
+
+    while(temp!=nullptr){
+        cout<<temp->value<<" ";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
+
+void begning(Node* &head, int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+int main (){
+    Node* node1 = new Node(1);
+    Node* node2 = new Node(2);
+    Node* node3 = new Node(3);
+    Node* node4 = new Node(4);
+    Node* node5 = new Node(5);
+    Node* node6 = new Node(6);
+
+    Node* head = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+
+    begning(head, -1);
+    iterate(head);
+    
+}
